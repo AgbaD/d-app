c737be4f8529f7d56d43e2398e25fc0362b3e16e
Damilare Agba <blankgodd33@gmail.com>
2022-11-16 08:57:13 +0100

    progress

--- src/contracts/BelSwap.sol
+++ src/contracts/BelSwap.sol
@@ -9,6 +9,20 @@ contract BelSwap {
     // 50 tokens to one etherium
     uint public rate = 100;
 
+    event TokensPurchased(
+        address account, 
+        address token,
+        uint amount,
+        uint rate
+    );
+
+    event TokensSold(
+        address account, 
+        address token,
+        uint amount,
+        uint rate
+    );
+
     constructor(Token _token) public {
         token = _token;
     }
@@ -16,6 +30,23 @@ contract BelSwap {
     function buyTokens() public payable {
         // amount of token to buy based on etherium rate
         uint tokenAmount = msg.value * rate;
+        require(token.balanceOf(address(this)) >= tokenAmount);
         token.transfer(msg.sender, tokenAmount);
+
+        // emit an event
+        emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+    function sellTokens(uint _tokenAmount) public {
+        // calculate amount of ether to be redeemed
+        uint ethAmount = _tokenAmount / rate;
+        require(address(this).balance >= ethAmount );
+        require(token.balanceOf(msg.sender) >= _tokenAmount);
+
+        token.transferFrom(msg.sender, address(this), _tokenAmount);
+        msg.sender.transfer(ethAmount);
+
+        // emit an event
+        emit TokensSold(msg.sender, address(token), _tokenAmount, rate);
     }
 }
--- test/BelSwap.test.js
+++ test/BelSwap.test.js
@@ -39,12 +39,12 @@ contract('BelSwap', ([deployer, investor]) => {
         })
     })
 
-    describe('BuyTokens', async () => {
+    describe('BuyTokens()', async () => {
         let result;
 
         // purchase token before running tests
         before(async() => {
-            await belSwap.buyTokens({from: investor, value: web3.utils.toWei('1', 'ether')})
+            result = await belSwap.buyTokens({from: investor, value: web3.utils.toWei('1', 'ether')})
         })
         it('users should be able to buy token from BelSwap', async () => {
             let investorBalance = await token.balanceOf(investor)
@@ -54,6 +54,40 @@ contract('BelSwap', ([deployer, investor]) => {
             assert.equal(belSwapBalance.toString(), tokens('999900'))
             belSwapBalance = await web3.eth.getBalance(belSwap.address)
             assert.equal(belSwapBalance.toString(), web3.utils.toWei('1', 'ether'))
+
+            const event = result.logs[0].args
+            assert.equal(event.account, investor)
+            assert.equal(event.token, token.address)
+            assert.equal(event.amount, tokens('100').toString())
+            assert.equal(event.rate.toString(), '100')
         })
     })
+
+    describe('SellTokens()', async () => {
+        let result;
+
+        // purchase token before running tests
+        before(async() => {
+            // investor must approve purchase
+            // must allow exchange to spend on their behals
+            await token.approve(belSwap.address, tokens('100'), {from: investor})
+            result = await belSwap.sellTokens(tokens('100'), {from: investor})
+        })
+        it('users should be able to sell token to BelSwap', async () => {
+            let investorBalance = await token.balanceOf(investor)
+            assert.equal(investorBalance.toString(), tokens('0'))
+
+            let belSwapBalance = await token.balanceOf(belSwap.address)
+            assert.equal(belSwapBalance.toString(), tokens('1000000'))
+            belSwapBalance = await web3.eth.getBalance(belSwap.address)
+            assert.equal(belSwapBalance.toString(), web3.utils.toWei('0', 'ether'))
+
+            const event = result.logs[0].args
+            assert.equal(event.account, investor)
+            assert.equal(event.token, token.address)
+            assert.equal(event.amount, tokens('100').toString())
+            assert.equal(event.rate.toString(), '100')
+        })
+    })
+
 })
